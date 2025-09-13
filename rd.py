e8ed32029f219fee82e4a8e0d7b4add576167bb8
BENAYYAD <abdessalam.benayyad@capgemini.com>
2025-09-14 01:42:16 +0200

    random module

---
+++ rd.py
@@ -0,0 +1,11 @@
+import random
+
+random_integer = random.randint(1,10)
+print(random_integer)
+
+random_float_between_0_1 = random.random()
+print(random_float_between_0_1)
+
+random_uniform = random.uniform(1,10)
+print(random_uniform)
+
